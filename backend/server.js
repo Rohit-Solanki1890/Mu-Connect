3bf73ae12f46b02c420278131d52704b07c6e17b
Rohit Solanki <rohitsolanki0476@gmail.com>
2025-10-26 03:31:29 +0530

    mongoose change for render environment uri change

--- backend/server.js
+++ backend/server.js
@@ -100,7 +100,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/marwadi-c
   process.exit(1);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.MONGO_URI || 5000;
 const HOST = '0.0.0.0';
 
 server.listen(PORT, HOST, () => {
